896a8949f8d57e8975d4077671e15f55a89a01a0
Pontax02 <ppontanilla07@gmail.com>
2025-04-24 14:15:01 +0200

    feat(App): Añadir codigo main

--- furnace/src/main/java/edu/estatuas/furnace/App.java
+++ furnace/src/main/java/edu/estatuas/furnace/App.java
@@ -1,10 +1,29 @@
 package edu.estatuas.furnace;
 
-/**
- * Hello world!
- */
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main( String[] args )
+    {
+        final double MIN_TEMP = 15.0;
+        final double MAX_TEMP = 21.0;
+
+        RoomTemperature temperature = RoomTemperature.getInstance();
+        temperature.setTemperature(17);
+        Heater heater = new GasHeater();
+        Thermometer thermometer = new RemoteCommandSensor();
+
+       // Controller controller = new Controller();
+
+        System.out.println( "Arrancando..." );
+        //controller.regulate(thermometer, heater, MIN_TEMP, MAX_TEMP, temperature);
+
+        //temperature = RoomTemperature.getInstance();
+        //Jedi yoda = new Jedi("Yoda");
+        //System.out.println( "\nArrancando a Yoda: " );
+        //regulator.regulate(thermometer, yoda, MIN_TEMP, MAX_TEMP, temperature);
+        //yoda.speak();
+
+        //Heater grogu = new Jedi("Grogu");
+        //((Jedi)grogu).speak(); // ups, The method speak() is undefined for the type HeaterJava
     }
 }
